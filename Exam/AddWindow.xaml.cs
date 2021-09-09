4f20c8d943d80912aebe7791fe12fed956a239a2
derta <derta@LAPTOP-K03E8FSF>
2021-09-09 17:52:34 +0500

    Добавьте файлы проекта.

---
+++ Exam/AddWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using Microsoft.Win32;
+
+namespace Exam
+{
+    /// <summary>
+    /// Логика взаимодействия для AddWindow.xaml
+    /// </summary>
+    public partial class AddWindow : Window
+    {
+        public AddWindow()
+        {
+            InitializeComponent();
+        }
+
+        public RecipeInfo recipe = new RecipeInfo();
+
+        private void Share_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog Dialog = new OpenFileDialog();
+            Dialog.Filter = "Фото(*.JPG;*.PNG;*.BMP)|*.JPG;*.PNG;*.BMP";
+            Dialog.CheckFileExists = true;
+            if (Dialog.ShowDialog() == true)
+            {
+                ImagePath.Text = Dialog.FileName;
+            }
+        }
+
+        private void AcceptButton_Click(object sender, RoutedEventArgs e)
+        {
+            recipe.name = Naming.Text;
+            recipe.path = ImagePath.Text;
+            recipe.Ingridients = Ingredients.Text;
+            recipe.Country = Culture.Text;
+            recipe.Recipe = new TextRange(RecipeBox.Document.ContentStart,RecipeBox.Document.ContentEnd).Text;
+            MessageBox.Show("Recipe Added");
+            this.Close();
+        }
+    }
+}
---
+++ Exam/MainWindow.xaml.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Exam
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = this;
+        }
+
+        List<RecipeInfo> infos = new List<RecipeInfo>();
+        int ID = 0;
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            AddWindow addWindow = new AddWindow();
+            addWindow.ShowDialog();
+            addWindow.recipe.id = ID;
+            ID++;
+            infos.Add(addWindow.recipe);
+            this.ListOfRecipes.ItemsSource = infos;
+            this.ListOfRecipes.Items.Refresh();
+            //ListBoxItem listBoxItem = new ListBoxItem();
+            // listBoxItem.Content = addWindow.recipe.name;     
+            // listBoxItem.Name = $"{ID}";
+            // ID++;
+
+            //ListOfRecipes.Items.Add(listBoxItem);
+        }
+
+        private void Delete_Click(object sender, RoutedEventArgs e)
+        {
+
+            infos.RemoveAt(this.ListOfRecipes.Items.IndexOf(this.ListOfRecipes.SelectedItem));
+            this.ListOfRecipes.Items.Refresh();
+        }
+    }
+
+    public class RecipeInfo
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string path { get; set; }
+        public string Ingridients { get; set; }
+        public string Country { get; set; }
+        public string Recipe { get; set; }
+
+        public override string ToString()
+        {
+            return this.name;
+        }
+    }
+}
